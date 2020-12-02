9bf0a54ab02c9c0482482e01f550dadfd6691c5c
willseven <wilsoncodoripairumani@gmail.com>
2020-12-01 22:28:30 -0400

    Add - authentication login, logout

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,8 @@ signupForm.addEventListener("submit", async (event) => {
 
     try {
         const {email, password} = getSignupFormInfo();
-        console.log(email,password);
+        await signup(email, password);
+        console.log('user created');
     } catch (ex) {
         alert("An error ocurred trying to signup: " + ex.message);
     } finally{
@@ -13,6 +14,7 @@ signupForm.addEventListener("submit", async (event) => {
     }
 });
 
+//para signup
 function getSignupFormInfo(){
     
     const email = signupForm["signup-email"].value;
@@ -20,3 +22,53 @@ function getSignupFormInfo(){
 
     return{ email, password };
 }
+//para login
+function getLoginFormInfo(){
+    
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
+
+    return{ email, password };
+}
+
+//metodos
+async function signup(email, password){
+    const creds = await auth.createUserWithEmailAndPassword(email, password);
+    //aqui es donde vamos a la coleccion de la base de datos 
+    return db
+    .collection("users")
+    .doc(creds.user.uid)
+    .set({
+        bio: signupForm["signup-bio"].value
+    });
+}
+
+async function login(email, password){
+    return await auth.signInWithEmailAndPassword(email, password);
+}
+
+async function logout(){
+    return await auth.signOut();
+}
+
+logoutButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    await logout();
+    console.log('User logged out!');
+})
+
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const modal = document.getElementById("modal-login");
+
+    try {
+        const {email, password} = getLoginFormInfo();
+        await login(email, password);
+        console.log('user logged in');
+    } catch (ex) {
+        alert("An error ocurred trying to signup: " + ex.message);
+    } finally{
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    }
+});
